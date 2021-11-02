9aba178a7e990a685174e4467a7b13ceebc0fc32
Hector <hdelgado9@outlook.com>
2021-11-01 23:27:49 -0600

    slideshow in progress

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 const menuBtn = document.querySelectorAll('.toggleMenu');
 const cartBtn = document.getElementById('cart-btn');
 const collection = document.querySelector('.collections');
+const slides = document.querySelectorAll('.slides');
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
 
 menuBtn.forEach(item => {
     item.addEventListener('click', () => {
         if(collection.clientWidth === 0) {
-            console.log('a')
             collection.style.width = "50%";
         }
         else {
             collection.style.width = "0";
         }
     });
-})
+});
+
+let slidesIndex = 1;
+prevBtn.addEventListener('click', ()=> {
+    
+});
+nextBtn.addEventListener('click', ()=> {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    if (slidesIndex > slides.length) {
+        slidesIndex = 1;
+    }
+    slides[slidesIndex-1].style.display = "block";
+});
